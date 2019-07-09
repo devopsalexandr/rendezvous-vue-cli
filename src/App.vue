0443810134e99ed79ee1784b5dd61609d2fb4777
Alexander <alexander@MacBook.Dlink>
2019-07-09 09:58:46 +0400

    base template added

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,33 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+  <html>
+
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  </head>
+
+  <body>
+
+  <nav-bar />
+
+  <div class="container">
     <router-view/>
   </div>
+  </body>
+
+  </html>
+
 </template>
 
+<script>
+  import NavBar from "./views/NavBar";
+
+  export default {
+    components: {
+      'NavBar' : NavBar
+    }
+  }
+</script>
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ src/views/NavBar.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+        <router-link :to="{name: 'home'}" tag="h5" class="my-0 mr-md-auto font-weight-normal">Rendezvous</router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "NavBar"
+    }
+</script>
+
+<style scoped>
+
+</style>
