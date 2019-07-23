416146654da0cf28ef14d6f1963861f46af8db1d
Alexander <alexander@MacBook.Dlink>
2019-07-23 16:04:34 +0400

    added router protect method

--- src/router.js
+++ src/router.js
@@ -7,48 +7,100 @@ import Profile from "./views/ProfileAuthUser";
 import ProfileById from "./views/ProfileById";
 import Search from "./views/Search";
 import Conversations from "./views/Conversations";
+import store from './store'
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: Home,
+      meta: {
+        title: 'Home'
+      }
     },
     {
       path: '/login',
       name: 'login',
-      component: Login
+      component: Login,
+      meta: {
+        title: 'Please SignIn',
+        requiresAuth: false
+      }
     },
     {
       path: '/register',
       name: 'register',
-      component: Register
+      component: Register,
+      meta: {
+        title: 'Registration',
+        requiresAuth: false
+      }
     },
     {
       path: '/profile',
       name: 'profile',
-      component: Profile
+      component: Profile,
+      meta: {
+        title: 'My profile',
+        requiresAuth: true
+      }
     },
     {
       path: '/profile/:id',
       name: 'profileById',
       component: ProfileById,
       props: true,
+      meta: {
+        requiresAuth: true
+      }
     },
     {
       path: '/search',
       name: 'search',
       component: Search,
+      meta: {
+        title: 'Search',
+        requiresAuth: true
+      }
     },
     {
       path: '/conversations',
       name: 'conversations',
       component: Conversations,
+      meta: {
+        title: 'Conversations',
+        requiresAuth: true
+      }
     },
   ]
-})
+});
+
+const DEFAULT_TITLE = ' :: ' + process.env.VUE_APP_NAME;
+
+router.beforeEach((to, from, next) => {
+
+  if(to.meta.title) {
+    document.title = to.meta.title + DEFAULT_TITLE;
+  }
+
+  store.dispatch('auth/checkStorageTokenExist').then(() => {
+    next();
+  }).catch(() => {
+
+    if(to.meta.requiresAuth){
+      next({name: 'login'});
+      return;
+    }
+
+    next();
+
+  });
+
+});
+
+export default router;
